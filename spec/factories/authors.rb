8b835275adcc3adb81d7a66663f6fbde65e4573f
bottles9 <alg70021@gmail.com>
2019-08-15 00:12:39 +0000

    add author & book model & cover uploader

---
+++ spec/factories/authors.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :author do
+    given_name "Henry"
+    family_name "Aovare"
+  end
+
+  factory :michael_hartl, class: Author do
+given_name 'Michael'
+family_name 'Hartl'
+end
+factory :sam_ruby, class: Author do
+given_name 'Sam'
+family_name 'Ruby'
+end
+end
